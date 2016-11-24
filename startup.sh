f3cab356013f9fdc348080d637f10a4e072157de
chobits <chobits@itri.org.tw>
2016-11-24 04:45:02 +0000

    check network status before start mavproxy

--- startup.sh
+++ startup.sh
@@ -16,11 +16,13 @@ touch /tmp/my_startup_lock
 #huawei fix
 #usb_modeswitch -v 12d1 -p 1f01 -c /usr/share/usb_modeswitch/12d1\:1f01
 
+service udhcpd start
+
 #huawei e3372h modem switch
 usb_modeswitch -s 10 -v 12d1 -p 14fe -V 12d1 -P 1506 -M '55534243123456780000000000000011062000000100000000000000000000'
-sleep 1
+sleep 3
 echo -e "AT^NDISDUP=1,1,\"internet\"\r" > /dev/ttyUSB0
-sleep 1
+sleep 3
 
 while true
 do
@@ -35,6 +37,17 @@ do
     sleep 1
 done
 
+while true
+do
+    ping -q -c1 www.google.com
+    if [ $? == 0 ]; then
+        break
+    fi
+    sleep 1
+done
+
+sudo -u pi tmux new-session -d -s hello 'cd /home/pi && mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:140.96.178.37:8090 --cmd="set source_system 250;set heartbeat 0;module load chobits" --moddebug=3'
+
 #wifi ap for iphone
 #ifconfig wlan0 10.1.1.1 netmask 255.255.255.0
 #sysctl net.ipv4.ip_forward=1
@@ -43,16 +56,16 @@ done
 
 #nat for ip camera
 sysctl net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o wwan0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.1.0/16 -o wwan0 -j MASQUERADE
 iptables -A FORWARD -i wwan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth0 -o wwan0 -j ACCEPT
-service udhcpd start
+#service udhcpd start
 
 #cd /home/pi/src/dtls_transport && /home/pi/src/dtls_transport/a.out -p 8100 140.96.178.37 &
 #sleep 10
 #python /home/pi/mqtt_client/MQTTSNclient_drone.py drone1 &
 #mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:127.0.0.1:14550 --daemon --cmd="set source_system 254;set heartbeat 0"
 #python /home/pi/src/scripts/mav_enc_fwd.py &
-mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:140.96.178.37:8090 --daemon --cmd="set source_system 250;set heartbeat 0" --load-module=chobits
+#sudo -u pi tmux new-session -d -s hello 'top'
+#mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:140.96.178.37:8090 --daemon --cmd="set source_system 250;set heartbeat 0" --load-module=chobits
 #mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:10.101.136.142:8090 --daemon --cmd="set source_system 250;set heartbeat 0" --load-module=chobits
-exit 0
