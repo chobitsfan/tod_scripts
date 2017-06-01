2478a3bf32680028d283e11aaefc28aa5368d6c0
chobits <chobits@itri.org.tw>
2017-06-01 08:13:24 +0000

    add dlink dwm222 support

--- startup.sh
+++ startup.sh
@@ -5,23 +5,34 @@ then
 fi
 touch /tmp/my_startup_lock
 
-#ifconfig wlan0 10.1.1.1 netmask 255.255.255.0
-#hostapd /etc/hostapd/iphone.conf &
+ifconfig wlan0 10.1.1.1 netmask 255.255.255.0
+hostapd /etc/hostapd/iphone.conf &
 #sleep 5
 
 #mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=10.1.1.10:14550 --daemon --cmd="set source_system 254;set heartbeat 0"
 
-#exit 0
+#sudo -u pi tmux new-session -d -s hello 'cd /home/pi/src/landing && python landing_test.py'
+#sudo -u pi tmux new-session -d -s hello 'cd /home/pi/src/computex && python trigger_takeoff.py'
+
+exit 0
 
 #huawei fix
 #usb_modeswitch -v 12d1 -p 1f01 -c /usr/share/usb_modeswitch/12d1\:1f01
 
-service udhcpd start
+#service udhcpd start
+
+#dlink dwm-222
+#usb_modeswitch -v 2001 -p ab00 -W -n -M '555342431060D30B000000000001061B000000020000000000000000000000'
+#modprobe qmi_wwan
+#modprobe option
+#echo "2001 7e35" > /sys/bus/usb-serial/drivers/option1/new_id
+#qmi-network /dev/cdc-wdm0 start
 
 #huawei e3372h modem switch
 usb_modeswitch -s 10 -v 12d1 -p 14fe -V 12d1 -P 1506 -M '55534243123456780000000000000011062000000100000000000000000000'
 sleep 3
-echo -e "AT^NDISDUP=1,1,\"internet\"\r" > /dev/ttyUSB0
+#echo -e "AT^NDISDUP=1,1,\"internet\"\r" > /dev/ttyUSB0
+echo -e "AT^NDISDUP=1,1,\"eclte\"\r" > /dev/ttyUSB0
 sleep 3
 
 while true
@@ -46,8 +57,8 @@ do
     sleep 1
 done
 
-#sudo -u pi tmux new-session -d -s hello 'cd /home/pi && mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:140.96.178.37:8090 --cmd="set source_system 250;set heartbeat 0;module load chobits" --moddebug=3'
-sudo -u pi tmux new-session -d -s hello 'cd /home/pi && mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:10.101.136.142:8090 --cmd="set source_system 250;set heartbeat 0;module load chobits" --moddebug=3'
+sudo -u pi tmux new-session -d -s hello 'cd /home/pi && mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:140.96.178.37:8090 --cmd="set source_system 250;set heartbeat 0;module load chobits" --moddebug=3'
+#sudo -u pi tmux new-session -d -s hello 'cd /home/pi && mavproxy.py --quadcopter --master=/dev/ttyAMA0 --out=udp:10.101.136.142:8090 --cmd="set source_system 250;set heartbeat 0;module load chobits" --moddebug=3'
 
 #wifi ap for iphone
 #ifconfig wlan0 10.1.1.1 netmask 255.255.255.0
@@ -56,13 +67,13 @@ sudo -u pi tmux new-session -d -s hello 'cd /home/pi && mavproxy.py --quadcopter
 #hostapd /etc/hostapd/iphone.conf &
 
 #nat for ip camera
-ifconfig wwan0
-if [ $? == 0 ]; then
-    sysctl net.ipv4.ip_forward=1
-    iptables -t nat -A POSTROUTING -o wwan0 -j MASQUERADE
-    iptables -A FORWARD -i wwan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-    iptables -A FORWARD -i eth0 -o wwan0 -j ACCEPT
-fi
+#ifconfig wwan0
+#if [ $? == 0 ]; then
+#    sysctl net.ipv4.ip_forward=1
+#    iptables -t nat -A POSTROUTING -o wwan0 -j MASQUERADE
+#    iptables -A FORWARD -i wwan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#    iptables -A FORWARD -i eth0 -o wwan0 -j ACCEPT
+#fi
 #service udhcpd start
 
 #cd /home/pi/src/dtls_transport && /home/pi/src/dtls_transport/a.out -p 8100 140.96.178.37 &
